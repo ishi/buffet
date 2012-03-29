2343c9aaf9e511af6eae1d95bc4cf1ad1ca2cba2
Ola Juchniewicz <aleksandra.juchniewicz@gmail.com>
2012-03-30 00:25:38 +0200

    linki plus szczegoly

--- application/controllers/NewsController.php
+++ application/controllers/NewsController.php
@@ -11,7 +11,15 @@ class NewsController extends Zend_Controller_Action
     public function indexAction()
     {
         $news = new Application_Model_NewsMapper();
-        $this->view->entries = $news->fetchAll();
+        $this->view->entries = $news->fetchAll("id");
+    }
+    
+    public function detailsAction()
+    {
+    	$news = new Application_Model_NewsMapper();
+    	$id = $this->_getParam('id', 1);
+        $this->view->entries = $news->fetchAll("id=$id");
+    	
     }
 
 
