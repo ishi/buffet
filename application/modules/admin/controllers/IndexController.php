df65624ed1fd332242cbf98f3129fbf87983c782
Radosław Zieliński <radoslaw@zielinski.cc>
2012-04-03 20:49:09 +0200

    Dodanie modułu administracyjnego

---
+++ application/modules/admin/controllers/IndexController.php
@@ -0,0 +1,33 @@
+<?php
+
+class Admin_IndexController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+    	$this -> _helper->layout()->setLayout("layout");
+        if(!isset($_SESSION['lang'])) $_SESSION['lang'] = $this->_getParam('lang', 'pl');
+        
+    	if ($_SESSION['lang']=='en'){
+        	$this -> _helper->layout()->setLayout("layout_en");
+        }
+        else{
+        	$this -> _helper->layout()->setLayout("layout");
+        }
+    }
+
+    public function indexAction()
+    {
+        // action body
+        //$this->view->tables = $this->getInvokeArg('bootstrap')->getResource('db')->listTables();
+        $home = new Application_Model_InformationMapper();
+        $this->view->entries = $home->fetchAll("type='home'");
+        
+        $picture = new Application_Model_PictureMapper();
+        $this->view->pictures = $picture->fetchAll("information='main'");
+    }
+    
+	
+
+}
+
