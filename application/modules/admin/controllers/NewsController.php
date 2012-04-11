6d4d91e127e6a832fa6a1662597504b106cf9657
Radosław Zieliński <radoslaw@zielinski.cc>
2012-04-11 22:10:31 +0200

    Dodanie pluginu umożliwiającego konfigurację layoutu w application.ini

--- application/modules/admin/controllers/NewsController.php
+++ application/modules/admin/controllers/NewsController.php
@@ -2,13 +2,6 @@
 
 class Admin_NewsController extends Zend_Controller_Action
 {
-
-    public function init()
-    {
-    	/**/
-    	$this -> _helper->layout()->setLayout("layout_admin");
-    }
-
     public function indexAction()
     {
         // action body
---
+++ library/Core/Layout/Controller/Plugin/ModuleLayout.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin odpowiedzialny za zmianę lauoutu w zależności od modułu
+ * 
+ * Ustawienia pobierane są z konfiguracji aplikacji.
+ * @author ishi
+ *
+ */
+class Core_Layout_Controller_Plugin_ModuleLayout extends Zend_Layout_Controller_Plugin_Layout {
+
+	public function preDispatch(Zend_Controller_Request_Abstract $request) {
+		$bootstrap = Zend_Controller_Front::getInstance()->getParam('bootstrap');
+		$config = $bootstrap->getOption('resources');
+		
+		if ($config = $this->getModuleConfig($config, $request->getModuleName())) {
+			$this->getLayout()->setOptions($config);
+		}
+	}
+	
+	private function getModuleConfig($config, $module) {
+		if (isset($config['layout']['module'][$module])) {
+			return $config['layout']['module'][$module];
+		}
+	}
+}
