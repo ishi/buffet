80bb9d748a2fe10e6fe30836a3dfd32af61d693d
Ola Juchniewicz <aleksandra.juchniewicz@gmail.com>
2012-04-11 22:14:40 +0200

    jakies zmiany nie pamietam jakie ;)

--- application/modules/admin/controllers/IndexController.php
+++ application/modules/admin/controllers/IndexController.php
@@ -6,7 +6,9 @@ class Admin_IndexController extends Zend_Controller_Action
     public function init()
     {
     	/**/
+
     	$this -> _helper->layout()->setLayout("layout_admin");
+    	
     }
 
     public function indexAction()
